c74946f9b8aad4bc6030ba71bd1a5359ac8b442a
MountainHills <antonbondoc14@gmail.com>
2022-04-10 17:25:47 +0800

    Initial Commit

---
+++ src/main/java/com/antonbondoc/AnoUlam/AnoUlamApplication.java
@@ -0,0 +1,13 @@
+package com.antonbondoc.AnoUlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnoUlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnoUlamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonbondoc/AnoUlam/AnoUlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonbondoc.AnoUlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoUlamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
